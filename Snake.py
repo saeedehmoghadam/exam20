0a00d766b9f114071c68354aaa87bafe2be59749
Saeideh Moghadam <88154810+saeedehmoghadam@users.noreply.github.com>
2021-10-31 23:49:30 +0330

    Add files via upload

--- Snake.py
+++ Snake.py
@@ -1,49 +1,44 @@
-import time
 import random
 import arcade
 
-height = 500
-width = 500
+HEIGHT = 600
+WIDTH = 600
 
 class Apple(arcade.Sprite):
     def __init__(self, w, h):
         arcade.Sprite.__init__(self)
-        self.image = 'apple-emoji-android-granny-smith-manzana-verde-green-apple.jpg'
-        self.apple = arcade.Sprite(self.image, 0.1)
-        self.apple.center_x = random.randint(25, w + 5)
-        self.apple.center_y = random.randint(25, h + 5)
+        self.apple = arcade.Sprite('apple.png', scale=0.05)
+        self.apple.center_x = random.randint(10, w - 10)
+        self.apple.center_y = random.randint(10, h - 10)
         
     def draw(self):
         self.apple.draw()
-       
+        
+class Cactus(arcade.Sprite):
+    def __init__(self, w, h):
+        arcade.Sprite.__init__(self)
+        self.cactus = arcade.Sprite('cactus.png', scale=0.2)
+        self.cactus.center_x = random.randint(10, w - 10)
+        self.cactus.center_y = random.randint(10, h - 10)
+        
+    def draw(self):
+        self.cactus.draw()
+    
 class Flower(arcade.Sprite):
     def __init__(self, w, h):
         arcade.Sprite.__init__(self)
-        self.image = 'R.jpg'
-        self.flower = arcade.Sprite(self.image, 0.1)
-        self.flower.center_x = random.randint(25, w + 5)
-        self.flower.center_y = random.randint(25, h + 5)
+        self.flower = arcade.Sprite('flower.png', scale=0.08)
+        self.flower.center_x = random.randint(10, w - 10)
+        self.flower.center_y = random.randint(10, h - 10)
         
     def draw(self):
         self.flower.draw()
 
-class Cactus(arcade.Sprite):
-    def __init__(self, w, h):
-        arcade.Sprite.__init__(self)
-        self.image = 'R (1).jpg'
-        self.cactus = arcade.Sprite(self.image, 0.1)
-        self.cactus.center_x = random.randint(25, w + 5)
-        self.cactus.center_y = random.randint(25, h + 5)
-        
-    def draw(self):
-        self.cactus.draw()
-
 class Snake(arcade.Sprite):
     def __init__(self, w, h):
         arcade.Sprite.__init__(self)
-        self.color2=arcade.color.BLUE
-        self.color3=arcade.color.YELLOW
-        self.color1=arcade.color.MAGENTA
+        self.color = arcade.color.BLUE
+        self.bodycolor = arcade.color.YELLOW
         self.speed = 1
         self.width = 16
         self.height = 16
@@ -59,10 +54,9 @@ class Snake(arcade.Sprite):
     def draw(self):
         for i in range(len(self.body)):
             if i == 0:
-                arcade.draw_circle_filled(self.body[i][0], self.body[i][1], self.r, self.color2)
+                arcade.draw_circle_filled(self.body[i][0], self.body[i][1], self.r, self.color)
             else:
-                arcade.draw_circle_outline(self.body[i][0], self.body[i][1], self.r, self.color3)
-            arcade.draw_circle_filled(self.center_x,self.center_y,self.r,self.color1)
+                arcade.draw_circle_outline(self.body[i][0], self.body[i][1], self.r, self.bodycolor)
 
     def move(self, appleX, appleY):
         self.change_x = 0
@@ -108,54 +102,39 @@ class Snake(arcade.Sprite):
     
 class Game(arcade.Window):
     def __init__(self):
-        arcade.Window.__init__(self, width, height, "Snake Game")
+        arcade.Window.__init__(self, WIDTH, HEIGHT, "Snake GAME")
         arcade.set_background_color(arcade.color.BLACK)
-        self.snake = Snake(width, height)
-        self.apple = Apple(width, height)
-        self.cactus = Cactus(width, height)
-        self.flower = Flower(width, height)
+        self.snake = Snake(WIDTH, HEIGHT)
+        self.apple = Apple(WIDTH, HEIGHT)
+        self.cactus = Cactus(WIDTH, HEIGHT)
+        self.flower = Flower(WIDTH, HEIGHT)
         
     def on_draw(self):
+        
         arcade.start_render()
-        if self.lose():
-            arcade.set_background_color(arcade.color.RED)
-            arcade.draw_text('Game Over', 160, 250, arcade.color.WHITE, 20, 20)
-            time.sleep(5)
-            return
         self.snake.draw()
         self.apple.draw()
-        self.flower.draw()
         self.cactus.draw()
-        # arcade.draw_text('SCORE : ', 50, height - 25, arcade.color.BLACK)
-        # arcade.draw_text(str(self.snake.score), 100, height - 25, arcade.color.BLACK, italic=True)
-
-
-        arcade.draw_text(text=f'Score: {self.snake.score}',start_x=0,start_y=460,width=500, font_size=20, align="center", color=arcade.color.WHITE)
-
-
-
+        self.flower.draw()
+        arcade.draw_text('Score : ', 20, HEIGHT - 25, arcade.color.GREEN)
+        arcade.draw_text(str(self.snake.score), 100, HEIGHT - 25, arcade.color.GREEN, italic=True)
+        
             
     def on_update(self, delta_time: float):
-      
+       
         self.snake.move(self.flower.flower.center_x, self.flower.flower.center_y)
         if arcade.check_for_collision(self.apple.apple, self.snake): 
             self.snake.eat(0)
-            self.apple = Apple(width, height)
+            self.apple = Apple(WIDTH, HEIGHT)
             
         if arcade.check_for_collision(self.cactus.cactus, self.snake): 
             self.snake.eat(1)
-            self.pooneh = Cactus(width, height)
+            self.cactus = Cactus(WIDTH, HEIGHT)
             
         if arcade.check_for_collision(self.flower.flower, self.snake): 
             self.snake.eat(2)
-            self.pear = Flower(width,height) 
-
-    def lose(self):
-        if self.snake.center_x>=490 or self.snake.center_x<=10 or self.snake.center_y>=490 or self.snake.center_y<=10 or self.snake.score<0:
-            return True
-        else:
-            return False        
+            self.flower = Flower(WIDTH, HEIGHT) 
         
-# if __name__ == '__main__':
-game = Game()
-arcade.run()
+if __name__ == '__main__':
+    game = Game()
+    arcade.run()
